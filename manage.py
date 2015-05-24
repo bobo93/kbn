5118360df3dba6ee56abfb44e0db161cd7ed7d5e
Patryk Mikołajczyk <mikpat92@gmail.com>
2015-05-24 22:46:36 +0200

    usunięty blog

--- manage.py
+++ manage.py
@@ -1,13 +1,12 @@
-#!/usr/bin/env python
 import os
 import sys
 
 if __name__ == "__main__":
-<<<<<<< HEAD
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "KBN1.settings")
-=======
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
->>>>>>> f40088e11f6ed6131d4291bf34da929df623c766
+
+    #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
 
     from django.core.management import execute_from_command_line
 
